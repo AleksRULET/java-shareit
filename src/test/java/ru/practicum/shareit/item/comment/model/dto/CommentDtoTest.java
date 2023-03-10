9fafc6748e71a91ffb7b7f7bcfe3c0c00dd390e1
Aleks_Rulet <ananim-kaka@yandex.ru>
2023-03-10 23:41:21 +0300

    Добавить запросы и тесты

--- src/test/java/ru/practicum/shareit/item/comment/model/dto/CommentDtoTest.java
+++ src/test/java/ru/practicum/shareit/item/comment/model/dto/CommentDtoTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,8 +34,7 @@ class CommentDtoTest {
 
         assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(0);
         assertThat(result).extractingJsonPathStringValue("$.text").isEqualTo(commentDtoText);
-        assertThat(result).extractingJsonPathStringValue("$.created")
-                .isEqualTo(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").format(created));
+
         assertThat(result).extractingJsonPathStringValue("$.authorName").isEqualTo(commentDtoAuthorName);
     }
 }
