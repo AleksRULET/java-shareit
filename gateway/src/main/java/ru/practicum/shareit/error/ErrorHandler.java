18621e4c5bf6f054b846465cf55c4f6a0e8a64eb
Aleks_Rulet <ananim-kaka@yandex.ru>
2023-03-11 16:19:45 +0300

    Исправить замечания

--- gateway/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,13 +31,13 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleThrowable(final Throwable validationException) {
+    public ErrorResponse handleThrowable(Throwable validationException) {
         log.error("500", validationException);
         validationException.printStackTrace();
         return new ErrorResponse("An unexpected error has occurred");
     }
 
-    @ExceptionHandler
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationAnnotation(ConstraintViolationException validationException) {
         log.warn(validationException.getMessage());
