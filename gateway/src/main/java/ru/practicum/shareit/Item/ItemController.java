ceccb33c80e60140fc216960bcc49a7928ec4a70
Aleks_Rulet <ananim-kaka@yandex.ru>
2023-03-11 16:32:13 +0300

    Исправить замечания

--- gateway/src/main/java/ru/practicum/shareit/Item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/Item/ItemController.java
@@ -62,9 +62,7 @@ public class ItemController {
     public Object search(@RequestParam String text,
                          @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
                          @Positive @RequestParam(defaultValue = "5") Integer size) {
-        if (text.isBlank()) {
-            return Collections.emptyList();
-        }
+
         log.info("GET : search items by text : {}", text);
         return itemClient.searchItems(text, from, size);
     }
