a0894201012e2ffad2018da55b68456c7c04e119
Aleks_Rulet <ananim-kaka@yandex.ru>
2023-03-12 02:05:27 +0300

    Исправить замечания

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 import lombok.ToString;
 import ru.practicum.shareit.validation.StartBeforeEndDateValid;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
